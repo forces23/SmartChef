e83fb86e35c8f326a8c385b6aa54fdfc6cdcbee3
Bobby Lawson <bobby.lawson17@gmail.com>
2025-04-26 19:36:29 -0500

    created the smartchef app

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,6 +6,11 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to the AI-Powered SmartChef",
+      verificationEmailBody: (createCode) => 
+        `Use this code to verify your account: ${(createCode())}`
+    }
   },
 });
