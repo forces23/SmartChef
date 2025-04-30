43505aad46403d33996585a12fce3b33622d2cfa
Bobby Lawson <bobby.lawson17@gmail.com>
2025-04-29 22:29:39 -0500

    added a signout button with no functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from "react";
 import { Loader, Placeholder, } from "@aws-amplify/ui-react";
 import "./App.css";
+import "./styles/buttons.scss";
 import { Amplify } from "aws-amplify";
 import { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
@@ -56,43 +57,49 @@ function App() {
   };
 
   return (
-    <div className="app-container">
-      <div className="header-container">
-        <h1 className="main-header">
-          Meet Your Personal
-          <br />
-          <span className="highlight">Smart Chef</span>
-        </h1>
-        <p className="description">
-          Simply type a few ingredients using the format ingredient1,
-          ingredient2, etc., and Recipe AI will generate an all-new recipe on
-          demand...
-        </p>
+    <div className="">
+      <div className="nav">
+        <button className="signout">Sign Out</button>
       </div>
-      <form onSubmit={onSubmit} className="form-container">
-        <div className="search-container">
-          <input
-            type="text"
-            className="wide-input"
-            id="ingredients"
-            name="ingredients"
-            placeholder="Ingredient1, Ingredient2, Ingredient3,...etc"
-          />
-          <button type="submit" className="search-button">Generate recipe</button>
+      <div className="app-container">
+        <div className="header-container">
+          <h1 className="main-header">
+            Meet Your Personal
+            <br />
+            <span className="highlight">Smart Chef</span>
+          </h1>
+          <p className="description">
+            Simply type a few ingredients using the format ingredient1,
+            ingredient2, etc., and Recipe AI will generate an all-new recipe on
+            demand...
+          </p>
         </div>
-      </form>
-      <div className="result-container">
-        {loading ? (
-          <div className="loader-container">
-            <p>Loading...</p>
-            <Loader size="large" />
-            <Placeholder size="large" />
-            <Placeholder size="large" />
-            <Placeholder size="large" />
+        <form onSubmit={onSubmit} className="form-container">
+          <div className="search-container">
+            <input
+              type="text"
+              className="wide-input"
+              id="ingredients"
+              name="ingredients"
+              placeholder="Ingredient1, Ingredient2, Ingredient3,...etc"
+            />
+            <button type="submit" className="search-button generate">Generate recipe</button>
+            
           </div>
-        ) : (
-          result && <p className="result">{result}</p>
-        )}
+        </form>
+        <div className="result-container">
+          {loading ? (
+            <div className="loader-container">
+              <p>Loading...</p>
+              <Loader size="large" />
+              <Placeholder size="large" />
+              <Placeholder size="large" />
+              <Placeholder size="large" />
+            </div>
+          ) : (
+            result && <p className="result">{result}</p>
+          )}
+        </div>
       </div>
     </div>
   );
